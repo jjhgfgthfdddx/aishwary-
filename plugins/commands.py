60cc3899d095bddbf04abca5150cc00d661c3c4e
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-10-27 09:48:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -75,7 +75,7 @@ async def send_file(client, query, ident, message, file_id):
     size = get_size(files.file_size)
     f_caption = files.file_name
     if CUSTOM_FILE_CAPTION:
-       try:
+        try:
             f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption, mention=message.from_user.mention)                
         except Exception as e:
             logger.exception(e)
